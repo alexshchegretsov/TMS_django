febb927b20013faa70c34b4fe87aeccc1ebbdce6
alex_shchegretsov <nydollz77@gmail.com>
2019-05-19 02:55:44 +0300

    Add own apps templates

--- src/cw_18/views.py
+++ src/cw_18/views.py
@@ -1,7 +1,11 @@
+from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def length_vowels_and_consonants(request):
+    if request.method == 'GET':
+        return render(request,'cw_18/comment_1.html')
     if request.method == 'POST':
         comment = request.POST.get('comm')
         vowels = 0
@@ -15,7 +19,10 @@ def length_vowels_and_consonants(request):
                     consonants += 1
         return HttpResponse(f'length comment: {len(comment)}{n}vowels: {vowels}{n}consonants: {consonants}')
 
+
 def split_comment(request):
+    if request.method == 'GET':
+        return render(request,'cw_18/split_comment.html')
     if request.method == 'POST':
         author = request.POST.get('first_name')
         list_comment = request.POST.get('comm').split('\r\n')
