e6f7a2eba21acc601adeb6d0ce2ba81cc23289fa
alex_shchegretsov <nydollz77@gmail.com>
2019-05-19 01:28:10 +0300

    Task split comment done

--- src/cw_18/urls.py
+++ src/cw_18/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import length_vowels_and_consonants
+from .views import length_vowels_and_consonants, split_comment
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('comment/', length_vowels_and_consonants),
+    path('comment/split/', split_comment),
 ]
--- src/cw_18/views.py
+++ src/cw_18/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def length_vowels_and_consonants(request):
-    comment = request.POST.get('comm')
-    vowels = 0
-    consonants = 0
-    n = '<br>'
-    for char in comment:
-        if char.isalpha():
-            if char.lower() in ['a', 'e', 'i', 'o', 'u']:
-                vowels += 1
-            else:
-                consonants += 1
-    return HttpResponse(f'length comment: {len(comment)}{n}vowels: {vowels}{n}consonants: {consonants}')
+    if request.method == 'POST':
+        comment = request.POST.get('comm')
+        vowels = 0
+        consonants = 0
+        n = '<br>'
+        for char in comment:
+            if char.isalpha():
+                if char.lower() in ['a', 'e', 'i', 'o', 'u']:
+                    vowels += 1
+                else:
+                    consonants += 1
+        return HttpResponse(f'length comment: {len(comment)}{n}vowels: {vowels}{n}consonants: {consonants}')
+
+def split_comment(request):
+    if request.method == 'POST':
+        author = request.POST.get('first_name')
+        list_comment = request.POST.get('comm').split('\r\n')
+        modified_list_comment = [f'{line} (c){author}' for line in list_comment]
+        return HttpResponse('<br>'.join(modified_list_comment))
