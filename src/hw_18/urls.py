42442841323d1d00a70e4d0e14bc33624ab1a77b
alex_shchegretsov <nydollz77@gmail.com>
2019-05-20 19:41:35 +0300

    task_3, task_4 done, added base html template

--- src/hw_18/urls.py
+++ src/hw_18/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import three_fields_handler
+from .views import (
+    three_fields_handler,
+    string_list_view,
+    clean_file
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('task_2/', three_fields_handler),
+    path('task_3/', string_list_view),
+    path('task_4/', clean_file),
 ]
--- src/hw_18/views.py
+++ src/hw_18/views.py
@@ -1,17 +1,30 @@
-from django.template import loader
+import os
 from django.shortcuts import render
-from django.http import HttpResponse
 
+# task 2
 def three_fields_handler(request):
     if request.method == 'GET':
-        return render(request,'hw_18/task_2.html')
+        return render(request, 'hw_18/task_2.html')
     if request.method == 'POST':
         data = request.POST
 
-        with open('file.txt','a') as file:
+        with open('file.txt', 'a') as file:
             for v in data.values():
                 file.write(f'{v.title()} ')
             file.write('\n')
         file = open('file.txt').readlines()
 
-        return render(request,'hw_18/form_task_2.html',context={'data_list': file})
+        return render(request, 'hw_18/form_task_2.html', context={'data_list': file})
+
+
+# task 3
+def string_list_view(request):
+    if request.method == 'GET':
+        file_list = open('file.txt').readlines()
+        return render(request,'hw_18/task_3.html',context={'file_list': file_list})
+
+# task 4
+def clean_file(request):
+    if request.method == 'GET':
+        os.remove(os.path.abspath('file.txt'))
+        return render(request,'hw_18/task_2.html')
