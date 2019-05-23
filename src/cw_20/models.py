dc18f21c3abbe137aa898f2c3e8957c15d9f1748
alex_shchegretsov <nydollz77@gmail.com>
2019-05-23 21:01:38 +0300

    Add requirements.txt

--- src/cw_20/models.py
+++ src/cw_20/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=200,null=False)
+    last_name = models.CharField(max_length=200, null=False)
+    profession = models.CharField(max_length=200, null=True)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
