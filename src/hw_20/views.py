7d7793be59c5a3998bd928429daa182664aff0af
alex_shchegretsov <nydollz77@gmail.com>
2019-05-26 14:17:38 +0300

    hw_20 Model Car CRUD done

--- src/hw_20/views.py
+++ src/hw_20/views.py
@@ -28,8 +28,41 @@ def create_car(request):
             return HttpResponse(f'{form.errors}')
     return HttpResponse('INVALID REQUEST METHOD')
 
+
 def edit_car(request, car_id):
-    pass
+    car = Car.objects.get(id=car_id)
+    if request.method == 'GET':
+        context = {
+            'car_id': car_id,
+            'form': CarForm(
+                initial={
+                    'brand': car.brand,
+                    'model': car.model,
+                    'color': car.color,
+                    'weight': car.weight,
+                    'owner_full_name': car.owner_full_name,
+                    'year_issue': car.year_issue,
+                }
+            )
+        }
+        return render(request, 'hw_20/edit.html', context)
+    elif request.method == 'POST':
+        form = CarForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Car.objects.filter(id=car_id).update(
+                brand=data.get('brand'),
+                model=data.get('model'),
+                color=data.get('color'),
+                weight=data.get('weight'),
+                owner_full_name=data.get('owner_full_name'),
+                year_issue=data.get('year_issue'),
+            )
+            return redirect('home_url')
+        else:
+            return HttpResponse(f'{form.errors}')
+
+    return HttpResponse('INVALID REQUEST')
 
 
 def delete_car(request, car_id):
